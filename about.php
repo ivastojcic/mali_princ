f8512835788cca50710410110d7ef9ef527e8041
ivastojcic <ivana.stojcic@fpmoz.sum.ba>
2019-09-13 13:22:39 +0200

    o nama

--- about.php
+++ about.php
@@ -11,16 +11,21 @@
       <main>
         <div class="jumbotron">
           <div class="container">
-            <h1 class="display-4">Hello, world!</h1>
+            <h1 class="display-4">O nama!</h1>
             <p class="lead">
-              This is a simple hero unit, a simple jumbotron-style component for
-              calling extra attention to featured content or information.
+            ADRESA : Kralja Petra Krešimira IV, Mostar 88000 <br>
+TELEFON:063/655-064 <br>
+e-mail: knj.maliprinc@gmail.com <br>
+
+RADNO VRIJEME: <br>
+pon-pet: 8:00-17:00 <br>
+sub: 8:00-12:00<br>
+<br>
+Knjižnica "Mali princ" smještena je na vrlo atraktivnom mjestu u gradu pokraj kojega mladi prolaze na putu za školu. Ideja je nastala u Ljubljani gdje se otvaraju male knjižnice koje odgovaraju raznovrsnim zahtjevima mladih. Knjižnica "Mali princ" je ugodan prostor jer podsjeća na dnevni boravak vlastite kuće. Ona je prava oaza za mlade ljude željne zanimljive literature i autorskih filmova, surfanja Internetom ili pak debatiranja o najrazličitijim temama uz ispijanje kave.
+Knjižnica "Mali princ" u Mostaru raspolaže sa prostorijom za računala kao i igraćim konzolama, te automatom za napitke za još bolji ugođaj.
             </p>
             <hr class="my-4" />
-            <p>
-              It uses utility classes for typography and spacing to space
-              content out within the larger container.
-            </p>
+            
             <a class="btn btn-primary btn-lg" href="Vizija.docx" role="button"
               >Vizija</a
             >
